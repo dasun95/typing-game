91ba5847f017d986ce545a3b8d3cc013657cbf04
Dasun95 <52498136+Dasun95@users.noreply.github.com>
2022-05-12 00:49:17 +0530

    added live progress bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { faker } from '@faker-js/faker';
 
-let randomTextII = faker.lorem.sentence();
 
 @Component({
   selector: 'app-root',
@@ -10,20 +9,54 @@ let randomTextII = faker.lorem.sentence();
 })
 export class AppComponent {
 
-  randomText = randomTextII;
-  isMatched = false;
-  typedText = '';
-  progress = '';
-  finished = false;
+  title = 'typing-game';
+
+  randomText: string = faker.lorem.sentence(5);
+  isMatched:boolean = false;
+  typedText:string = '';
+  progress: string = '';
+  
+  finished: boolean = false;
+
+  targetTime:number = this.randomText.length*500; 
+  startTime = Date.now();
+  
+  timer: any;
+ 
+
+  onStartGame() {
+
+    let startButton = (document.getElementById('startButton') as HTMLButtonElement)
+    startButton.disabled = true;
+    
+    let input = (document.getElementById("textInput") as HTMLInputElement);
+    input.disabled = false;
+
+    const timerTemp = setInterval(function (startTime: any, maxTime: any) {
+      let filled = document.getElementById('progress-bar') as HTMLProgressElement
+      filled.value = Date.now() - startTime;
+      console.log('failed :'+filled.value + 'max time'+ maxTime);
+      if (filled.value >= maxTime) {
+        clearInterval(timerTemp);
+        let display = (document.getElementById('progress') as HTMLHeadingElement);
+        display.innerHTML = 'GAME OVER!!';
+        // display.style();
+      }
+    }, 1000, this.startTime, this.targetTime);
+    this.timer = timerTemp;
+  }
+  
 
   onInput(value: string) {
+
+    let input = (document.getElementById("textInput") as HTMLInputElement);
 
     this.typedText = value;
 
     this.isMatched = value == this.randomText.slice(0, value.length);
 
     if (this.isMatched) {
-      let newParagraph = this.randomText.slice(value.length);
+      let newParagraph:string = this.randomText.slice(value.length);
       (document.getElementById('text') as HTMLElement).innerHTML = newParagraph;
       this.progress = 'You are doing great!';
     } else {
@@ -34,14 +67,14 @@ export class AppComponent {
 
     if (this.randomText==this.typedText) {
       this.finished = true;
-      const input = (document.getElementById("textInput") as HTMLElement)
-      input.setAttribute('disabled','');
+      input.disabled = true;
+      clearInterval(this.timer);
     }
     
   }
 
   onReset() {
-    location.reload()
+    location.reload();
   }
 
 }
